d2cd3b2b687c4bd5a0bd5cf6f84122b1146117f9
Nasywa Farahiya Prastiwi <nasywafarahiya@gmail.com>
2025-11-06 16:51:04 +0700

    nambah logika login

--- app/src/main/java/com/example/aplikasiandroidpertama/MainActivity.kt
+++ app/src/main/java/com/example/aplikasiandroidpertama/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.aplikasiandroidpertama
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +14,28 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val inputUsername = findViewById<EditText>(R.id.EditTextUsername)
+        val inputPassword = findViewById<EditText>(R.id.EditTextPassword)
+
+        val buttonSubmit = findViewById<Button>(R.id.buttonsubmit)
+
+
+        var username = inputUsername. text.toString()
+        var password = inputPassword. text.toString()
+
+        if(username.isEmpty() || password.isEmpty()) {
+            Toast.makeText(
+                this, "Username/Password tidak boleh kosong",
+                Toast.LENGTH_LONG)
+                .show()
+        }else{
+            Toast.makeText(this, "proces login..",
+            Toast.LENGTH_LONG)
+                .show()
+
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
